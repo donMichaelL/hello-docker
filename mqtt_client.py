cd0fe07c9fae69ff3c811a4843a45a4b4b4ecb0a
Michael <michael.loukeris@icloud.com>
2015-11-27 04:44:40 -0800

    Testing Jenkins 2

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,7 +1,7 @@
 import time
 import paho.mqtt.publish as publish
 
-print "That's one small step for man, one giant leap for mankind" 
+print "That's one small step for man, one giant leap for mankind!!!!" 
 # for i in range(0, 10):
 #         print i
 #         publish.single("airsoul", "%s This is a new Message" %(i), hostname="195.134.67.40",
